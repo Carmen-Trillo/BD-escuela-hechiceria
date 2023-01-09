678f0a102a2a428d712d2e8c812b0665a791beab
Carmen-Trillo <mctrillon@gmail.com>
2023-01-09 11:11:32 +0100

    updated page

--- __test__/validateInput.test.js
+++ __test__/validateInput.test.js
@@ -23,20 +23,3 @@ test("test validarInput function", () => {
     expect(validate.validarInput(testValueList[7])).toBe(false);
     expect(validate.validarInput(testValueList[8])).toBe(false);
 });
-
-test("test validarSelect function", () => {
-    let testSelectList = [
-        "Primero",
-        "Segundo",
-        "21/12/1980",
-        "05/09/1956",
-        "",
-    ];
-    
-    expect(validate.validarSelect(testSelectList[0])).toBe(true);
-    expect(validate.validarSelect(testSelectList[1])).toBe(true);
-    expect(validate.validarSelect(testSelectList[2])).toBe(true);
-    expect(validate.validarSelect(testSelectList[3])).toBe(true);
-    expect(validate.validarSelect(testSelectList[4])).toBe(false);
-
-});
